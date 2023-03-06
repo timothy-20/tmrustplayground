78494e022b80f55fae595f45d3900ae9eb1a5589
timothy <jungun3329@gmail.com>
2023-03-06 18:01:59 +0900

    230306) - 3월 6일분 학습 내용.

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@ extern crate tmrustplayground;
 
 use std::{env, process};
 use std::cell::Cell;
+use std::fmt::{Debug, Display};
+use std::iter::Enumerate;
 use std::ops::Range;
+use std::ptr::addr_of_mut;
 use tmrustplayground::{cache, minigrep};
 use tmrustplayground::minigrep::Config;
 
 use std::thread;
 use std::time:: Duration;
 use std::sync::mpsc;
+use std::vec::Drain;
 
 fn run_clt() {
     let config = Config::new(env::args()).unwrap_or_else(|error| {
@@ -22,48 +26,42 @@ fn run_clt() {
     }
 }
 
-fn execute<T>(tasks: Vec<fn() -> T>, is_multiple: bool) {
-    let (sender, receiver) = mpsc::channel();
-
-    if is_multiple {
-        for value in distribute_tasks(&tasks) {
-            if let Err(error) = sender.send(value) {
-                eprintln!("Unable to send task's result. Error description: {}", error);
-                break
-            }
-        }
-    } else {
-
-    }
-}
-
-fn distribute_tasks<T>(tasks: &Vec<fn() -> T>) -> Vec<T> {
-    if tasks.len() == 1 {
-        if let Some(task) = tasks.first() {
-            return vec![task()];
-        }
-        return vec![]
-    }
-
-    let half_index = tasks.len() / 2;
-    let mut result = vec![];
-
-    for i in (0..=tasks.len()).step_by(half_index) {
-        result.append(&mut distribute_tasks(&Vec::from(&tasks[i..=(i+half_index)])));
-    }
-
-    result
-}
+// #[derive(Copy, Clone)]
+// struct ForkAndJoin<T, F>
+//     where F: Fn() -> T {
+//     completion: F,
+//     value: T
+// }
+//
+// impl<T, F> ForkAndJoin<T, F>
+//     where F: Fn() -> T {
+//
+// }
+//
+// fn distribute_tasks<T, F>(mut tasks: Vec<F>)
+//     where T: Debug + Display + Clone,
+//           F: Fn() -> T + Clone {
+//     if tasks.len() == 1 {
+//         if let Some(task) = tasks.first() {
+//             eprintln!("Result: {}", (*task)());
+//         }
+//         return;
+//     }
+//
+//     let half_index = tasks.len() / 2;
+//     let first_half = vec![];
+//     let last_half = vec![];
+//
+//     tasks[..half_index].clone_from_slice(&first_half);
+//     tasks[half_index..].clone_from_slice(&last_half);
+//     distribute_tasks(first_half);
+//     distribute_tasks(last_half);
+// }
 
 fn main() {
-    let mut tasks = vec![];
+    let mut list = vec![1, 2, 3, 4, 5];
 
-    for i in 1..=10 {
-        tasks.push(|| {
-            thread::sleep(Duration::from_secs_f32(i as f32 * 0.1));
-            i
-        });
+    for (index, value) in list.iter().enumerate() {
+        eprintln!("{} is at index {}", value, index);
     }
-
-    distribute_tasks(&tasks);
 }
