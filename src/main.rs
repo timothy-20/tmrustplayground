999d589532239c39fad7e67cb47ba567b42015f7
timothy <jungun3329@gmail.com>
2023-03-07 19:04:02 +0900

    230307) - rust의 전반적인 문법 학습 완료. - 멀티 스레드 웹 서버 구현 파트 실습 중.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 extern crate tmrustplayground;
+extern crate core;
 
+use core::slice;
 use std::{env, process};
 use std::cell::Cell;
-use std::fmt::{Debug, Display};
+use std::fmt::{Debug, Display, Error, Formatter, write};
 use std::iter::Enumerate;
-use std::ops::Range;
+use std::ops::{Add, Range};
+use std::os::unix::process::parent_id;
 use std::ptr::addr_of_mut;
 use tmrustplayground::{cache, minigrep};
 use tmrustplayground::minigrep::Config;
@@ -48,20 +51,136 @@ fn run_clt() {
 //         return;
 //     }
 //
-//     let half_index = tasks.len() / 2;
-//     let first_half = vec![];
-//     let last_half = vec![];
+//     let tasks_ref = &mut tasks[..];
+//     let (first, last) = tasks_ref.split_at_mut(tasks_ref.len() / 2);
 //
-//     tasks[..half_index].clone_from_slice(&first_half);
-//     tasks[half_index..].clone_from_slice(&last_half);
-//     distribute_tasks(first_half);
-//     distribute_tasks(last_half);
+//     distribute_tasks(Vec::from(first));
+//     distribute_tasks(Vec::from(last));
 // }
 
-fn main() {
-    let mut list = vec![1, 2, 3, 4, 5];
+// fn split_at_mut<T>(slice: &mut [T], mid: usize) -> Vec<&mut [T]> {
+//     let length = slice.len();
+//     let p_slice = slice.as_mut_ptr();
+//
+//     assert!(mid <= length);
+//
+//     unsafe {
+//         vec![
+//             slice::from_raw_parts_mut(p_slice, mid),
+//             slice::from_raw_parts_mut(p_slice.offset(mid as isize), length - mid)
+//         ]
+//     }
+// }
 
-    for (index, value) in list.iter().enumerate() {
-        eprintln!("{} is at index {}", value, index);
+struct Context<'c>(&'c str);
+struct Parser<'p, 'c: 'p> {
+    context: &'p Context<'c>,
+}
+
+impl<'p, 'c> Parser<'p, 'c> {
+    fn parser(&self) -> Result<(), &'c str> {
+        Err(&self.context.0[1..])
     }
+}
+
+/// # Example
+/// ``` rust
+/// if let Err(error) = parse_context(Context("Hello, timothy")) {
+///     eprintln!("Error description: {}", error);
+/// }
+/// ```
+fn parse_context(context: Context) -> Result<(), &str> {
+    Parser { context: &context }.parser()
+}
+
+/// # Example
+/// ``` rust
+/// let num = 5;
+/// let obj = Box::new(Ball { diameter: &num }) as Box<dyn Red>;
+///
+/// (*obj).highlight();
+/// ```
+trait Red {
+    fn highlight(&self);
+}
+
+struct Ball<'a> {
+    diameter: &'a i32,
+}
+
+impl<'a> Red for Ball<'a> {
+    fn highlight(&self) {
+        println!("Highlighted!");
+    }
+}
+
+trait OutlinePrint: Display {
+    /// # Example
+    /// ``` rust
+    /// let point = Point { x: 10, y: 20 };
+    ///
+    /// point.outline_print();
+    /// ```
+    fn outline_print(&self) {
+        let output = self.to_string();
+        let length = output.len();
+
+        println!("{}", "*".repeat(length + 4));
+        println!("*{}*", " ".repeat(length + 2));
+        println!("* {} *", output);
+        println!("*{}*", " ".repeat(length + 2));
+        println!("{}", "*".repeat(length + 4));
+    }
+}
+
+#[derive(PartialEq)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+impl Add for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Point {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y
+        }
+    }
+}
+
+impl OutlinePrint for Point {}
+
+impl Display for Point {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+
+    fn add(self, rhs: Meters) -> Self::Output {
+        Millimeters(self.0 + rhs.0 * 1000)
+    }
+}
+
+struct Wrapper(Vec<String>);
+
+impl Display for Wrapper {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
+    }
+}
+
+fn return_closure(x: i32) -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x * 10)
+}
+
+fn main() {
+
 }
